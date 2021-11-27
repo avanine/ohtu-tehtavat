950d08bf97e07fa9097f1acdb9617f8d0adb501d
avanine <marike.puranen@gmail.com>
2021-11-27 20:13:08 +0200

    yhden tuotteen lisäämisen jälkeen ostoksen nimi ja lkm oikein

--- viikko4/TddOstoskori/src/test/java/ohtu/OstoskoriTest.java
+++ viikko4/TddOstoskori/src/test/java/ohtu/OstoskoriTest.java
@@ -18,7 +18,7 @@ public class OstoskoriTest {
 
     // step 1
     @Test
-    public void ostoskorinHintaJaTavaroidenMaaraAlussa() { 
+    public void ostoskorinHintaJaTavaroidenMaaraAlussa() {
         assertEquals(0, kori.hinta());
         assertEquals(0, kori.tavaroitaKorissa());
     }
@@ -26,78 +26,88 @@ public class OstoskoriTest {
     @Test
     public void yhdenTuotteenLisaamisenJalkeenKorissaYksiTuote() {
         Tuote maito = new Tuote("maito", 3);
- 
+
         kori.lisaaTuote(maito);
 
         assertEquals(1, kori.tavaroitaKorissa());
- 
+
     }
 
     @Test
     public void yhdenTuotteenLisaamisenJalkeenOstoskorinHintaSamaKuinTuotteenHinta() {
         Tuote maito = new Tuote("maito", 3);
- 
+
         kori.lisaaTuote(maito);
 
         assertEquals(3, kori.hinta());
- 
+
     }
 
     @Test
     public void kahdenEriTuotteenLisaamisenJalkeenOstoskorissaOnKaksiTavaraa() {
         Tuote maito = new Tuote("maito", 3);
         Tuote leipa = new Tuote("leipä", 4);
- 
+
         kori.lisaaTuote(maito);
         kori.lisaaTuote(leipa);
 
         assertEquals(2, kori.tavaroitaKorissa());
- 
+
     }
 
     @Test
     public void kahdenEriTuotteenLisaamisenJalkeenOstoskorinHintaOikea() {
         Tuote maito = new Tuote("maito", 3);
         Tuote leipa = new Tuote("leipä", 4);
- 
+
         kori.lisaaTuote(maito);
         kori.lisaaTuote(leipa);
 
         assertEquals(7, kori.hinta());
- 
+
     }
-    
+
     @Test
     public void kahdenSamanTuotteenLisaamisenJalkeenOstoskorissaOnKaksiTavaraa() {
         Tuote maito = new Tuote("maito", 3);
- 
+
         kori.lisaaTuote(maito);
         kori.lisaaTuote(maito);
 
         assertEquals(2, kori.tavaroitaKorissa());
- 
+
     }
 
     @Test
     public void kahdenSamanTuotteenLisaamisenJalkeenOstoskorinHintaOnOikea() {
         Tuote maito = new Tuote("maito", 3);
- 
+
         kori.lisaaTuote(maito);
         kori.lisaaTuote(maito);
 
         assertEquals(6, kori.hinta());
- 
+
     }
 
     @Test
     public void yhdenTuotteenLisaamisenJalkeenKorissaYksiOstosOlio() {
         Tuote maito = new Tuote("maito", 3);
         kori.lisaaTuote(maito);
- 
+
         List<Ostos> ostokset = kori.ostokset();
- 
+
         // testaa että metodin palauttamin listan pituus 1
         assertEquals(1, ostokset.size());
     }
 
+    @Test
+    public void yhdenTuotteenLisaamisenJalkeenKorissaYksiOstosOlioJollaOikeaTuotteenNimiJaMaara() {
+        Tuote maito = new Tuote("maito", 3);
+        kori.lisaaTuote(maito);
+
+        Ostos ostos = kori.ostokset().get(0);
+
+        assertEquals("maito", ostos.tuotteenNimi());
+        assertEquals(1, ostos.lukumaara());
+    }
 }
